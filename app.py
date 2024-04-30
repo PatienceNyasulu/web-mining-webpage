00cb5513d7d42d416f2fc15d389662b9e0f1a983
Czennie <83468952+PatienceNyasulu@users.noreply.github.com>
2024-04-30 13:04:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ st.title('WebMining Movie Clusters ')
 st.text('Praise Ganyiwa')
 st.text('Shamiso Makainganwa')
 st.text('Gamuchirai Nyasulu')
+st.text('Link to github code: https://github.com/PatienceNyasulu/web-mining-webpage ')
 
 # Let the user choose a cluster number
 st.header('Enter a Cluster')
@@ -45,68 +46,3 @@ for _, row in cluster_movies.head(6).iterrows():
     st.text(f"Description: {row['description']}")
     st.text(f"Listed In: {row['listed_in']}")
     st.text('---')  # Separator for better readability
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import requests
-
-def fetch_poster(movie_id):
-     url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
-     data=requests.get(url)
-     data=data.json()
-     poster_path = data['poster_path']
-     full_path = "https://image.tmdb.org/t/p/w500/"+poster_path
-     return full_path
-
-
-movie=pickle.load(open('kmeans_model.pkl', 'rb'))
-movie=pd.DataFrame(movie)
-#cluster_list = movie["Cluster"].values
-    
-    
-    # getting the input data from the user
-col1 = st.columns(1)
-    
-
-cluster = st.text_input('Enter Cluster 1-12')
-        
-
-        
-    # code for dispay
-res = ''
-
-
-#st.selectbox("select cluster",cluster_list)
-# Display movies by cluster
-
-
-
-if st.button("show results"):
-    
-    movie_array = []
-    res=movie_array
-    cluster_id = cluster
-    cluster_movies = movie[movie['cluster_label'] == cluster_id]['title']
-
-    if not cluster_movies.empty:
-        print(f"Movies in Cluster {cluster_id}:")
-        for movie in cluster_movies:
-           movie_array.append(movie)
-           
-    else:
-        res=print(f"No movies found in Cluster {cluster_id}")
-
-st.success(res)
